5242067f37f6e88edc44c912c8e21308a2c80228
kdiluca <kristen@mapzen.com>
2016-06-16 13:43:36 -0400

    added zinc-transit to optimized demo

--- optimized_route/js/optimized_route.js
+++ optimized_route/js/optimized_route.js
@@ -72,7 +72,7 @@ app.controller('OptimizedRouteController', function($scope, $rootScope, $sce, $h
   var road = L.tileLayer('http://b.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution : '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributers'
   }), zinc_transit = Tangram.leafletLayer({
-    scene: 'https://raw.githubusercontent.com/kdiluca/route-tester/gh-pages/routing/map_style/zinc-transit.yaml',
+    scene: 'https://raw.githubusercontent.com/valhalla/demos/gh-pages/routing/map_style/zinc-transit.yaml',
     attribution: '<a href="https://mapzen.com/tangram">Tangram</a> | &copy; OSM contributors | <a href="https://mapzen.com/">Mapzen</a>'
   }), cycle = L.tileLayer('http://b.tile.thunderforest.com/cycle/{z}/{x}/{y}.png', {
     attribution : 'Maps &copy; <a href="http://www.thunderforest.com">Thunderforest, </a>;Data &copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
